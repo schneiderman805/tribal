9c99d96c5663e7553ba21ae37ea1032c4503d9a3
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-30 09:15:04 +0100

    Addresses PR comments related to formating

--- client/bundles/Main/components/App.jsx
+++ client/bundles/Main/components/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import {Route, Switch, BrowserRouter} from 'react-router-dom'
-import Main from './Main'
-
+import React from "react";
+import {Route, Switch, BrowserRouter} from "react-router-dom";
+import Main from "./Main";
 
 const App = props => {
-	let passdown = props
+	let passdown = props;
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route exact path ='/' render={props => <Main {...props} props={passdown} />} />
+				<Route exact path ='/' render={props => <Main {...props} props={passdown} />} />;
 			</Switch>
 		</BrowserRouter>
-	)
-}
+	);
+};
 
-export default App
+export default App;
