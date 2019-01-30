711a1148fc713e476328488b73d7da992741f846
johannaanden <johannaanden@hotmail.com>
2019-01-30 12:31:11 +0100

    Fixes tailwind implementation and adds background color and font color

--- client/bundles/Main/components/Main.jsx
+++ client/bundles/Main/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AllSessions from "./AllSessions";
+
 export default class Main extends React.Component {
 	constructor(props) {
 		super(props);
@@ -12,20 +13,31 @@ export default class Main extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<div id='available'>
-					<h1>Available sessions</h1>
-					<AllSessions sessionlist={this.state.available_sessions} />
-					<br />
+			<div className='main_grid'>
+				<div className='header'>
+					<p>logo</p>
+					<p>Tribal</p>
 				</div>
-				<div id='booked'>
-					<h1>Booked sessions</h1>
-					<AllSessions sessionlist={this.state.booked_sessions} />
-					<br />
+				
+				<div className='content'>
+					<div id='available'>
+						<h1>Available sessions</h1>
+						<AllSessions sessionlist={this.state.available_sessions} />
+						<br />
+					</div>
+					<div id='booked'>
+						<h1>Booked sessions</h1>
+						<AllSessions sessionlist={this.state.booked_sessions} />
+						<br />
+					</div>
+					<div id='full'>
+						<h1>Full sessions</h1>
+						<AllSessions sessionlist={this.state.full_sessions} />
+					</div>
 				</div>
-				<div id='full'>
-					<h1>Full sessions</h1>
-					<AllSessions sessionlist={this.state.full_sessions} />
+
+				<div className='footer'>
+					<p>FOOTER</p>
 				</div>
 			</div>
 		);
