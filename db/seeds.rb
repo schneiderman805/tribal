11d8c73fb3736d6b7c491261ac690ea0eb30eb66
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-31 20:04:24 +0100

    Adds price table variable to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
+pricetable = PriceTable.new(trainees: 4, price_point: 0)
+
 5.times do 
-    Session.create(title: 'Crossfit', start_date: '2019-02-01 15:00:00', end_date: '2019-02-01 15:30:00', status: 0)
+    Session.create(title: 'Crossfit', start_date: '2019-02-01 15:00:00', end_date: '2019-02-01 15:30:00', status: 0, price_table_id: 1)
 end
 
 5.times do 
-    Session.create(title: 'Body Pump', start_date: '2019-02-01 15:00:00', end_date: '2019-02-01 15:30:00', status: 1)
+    Session.create(title: 'Body Pump', start_date: '2019-02-01 15:00:00', end_date: '2019-02-01 15:30:00', status: 1, price_table_id: 1)
 end
 
 5.times do 
-    Session.create(title: 'Yoga', start_date: '2019-02-01 15:00:00', end_date: '2019-02-01 15:30:00', status: 2)
+    Session.create(title: 'Yoga', start_date: '2019-02-01 15:00:00', end_date: '2019-02-01 15:30:00', status: 2, price_table_id: 1)
 end
