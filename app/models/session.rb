5ac31af55794a24b0df769d61b32d1f081873e4b
hanna_nyman <hannanyman.sk@gmail.com>
2019-02-01 10:36:14 +0100

    Generates sessions controller and sessions views

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,12 @@ class Session < ApplicationRecord
 	validates_presence_of :title, :start_date
 	belongs_to :price_table
 	enum status: { available: 0, booked: 1, full: 2 }
+
+	has_many :coaches, :through => :bookings
+
+	has_many :trainees, :through => :bookings
+	
+	has_many :bookings, :inverse_of => :sessions
+
+	accepts_nested_attributes_for :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_01_31_090257) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "price_tables", force: :cascade do |t|
